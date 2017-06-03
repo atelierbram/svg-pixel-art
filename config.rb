afe364ba881a331875e37c03e3d188bba13202b8
atelierbram <bramdeh@gmail.com>
2017-06-03 20:57:13 +0200

    minify css

--- config.rb
+++ config.rb
@@ -1,38 +1,27 @@
-# https://github.com/ai/autoprefixer
 require 'autoprefixer-rails'
-require 'csso'
 
 on_stylesheet_saved do |file|
   css = File.read(file)
-  File.open(file, 'w') do |io|
-#    io << Csso.optimize( AutoprefixerRails.compile(css) )
-    # io << ( AutoprefixerRails.compile(css) )
-    io << ( AutoprefixerRails.process(css) )
+  map = file + '.map'
+
+  if File.exists? map
+    result = AutoprefixerRails.process(css,
+      from: file,
+      to:   file,
+      map:  { prev: File.read(map), inline: false })
+    File.open(file, 'w') { |io| io << result.css }
+    File.open(map,  'w') { |io| io << result.map }
+  else
+    File.open(file, 'w') { |io| io << AutoprefixerRails.process(css) }
   end
 end
-
+ 
 http_path = "/"
 css_dir = "assets/css"
 sass_dir = "assets/sass"
 images_dir = "assets/img"
 javascripts_dir = "assets/js"
 relative_assets = true
-line_comments = true
-output_style = :expanded
-# output_style = :compressed
-
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
-
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+line_comments = false
+# output_style = :expanded
+output_style = :compressed
--- php/inc/config.php
+++ php/inc/config.php
@@ -1,2 +1,2 @@
 <?php // $rootUrl="http://localhost/~bram/github.com/atelierbram/svg-pixel-art/"; ?>
-<?php $rootUrl="http://atelierbram.github.io/svg-pixel-art/"; ?>
+<?php $rootUrl="//atelierbram.github.io/svg-pixel-art/"; ?>
